0d780f932a35dfff150c84c60c76664454a86493
Bird <39280243+jlambert360@users.noreply.github.com>
2021-07-13 13:43:09 -0400

    Update Quick 2GB SD.bat

--- VSDSync/bin/Quick 2GB SD.bat
+++ VSDSync/bin/Quick 2GB SD.bat
@@ -1,14 +1,9 @@
 @echo off
-MODE 30,2
 Title Quick 2GB SD
 
-:confirm
-color 5d
-echo.
 echo  Creating a 2GB SD card. . .
 "%~dp0mksdcard.exe" 2048M "sd.raw"
 move "sd.raw" "D:\a\WomBuild\WomBuild\Dolphin\User\Wii\"
-color 3b
 echo.
 echo  Created.
 
